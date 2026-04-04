04158734121509801d83e81336789d0343d19c7a
manuel <mn07@gmx.at>
2026-04-04 17:36:55 +0200

    fix: update pytest

--- test/test_weather.py
+++ test/test_weather.py
@@ -5,7 +5,8 @@ from src.meteo import get_forecast
 def test_openmeteo():
     lat = 47.07
     lon = 15.44
+    altitude = "666"
     name = "Graz"
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    assert get_forecast(lat, lon, name, now)
+    assert get_forecast(lat, lon, altitude, name, now)
